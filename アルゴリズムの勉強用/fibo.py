b1410959ee5961d1b87dc13da5b095ac747779a3
gegeson <yushin1301@gmail.com>
2021-02-18 09:54:15 +0900

    アルゴリズムの勉強　フィボナッチ追加

---
+++ アルゴリズムの勉強用/fibo.py
@@ -0,0 +1,13 @@
+# 動的計画法（メモ化）することでforで回すより早くなる
+def fibo(n, memo={}):
+    if n == 1:
+        return 0
+    elif n == 2:
+        return 1
+    elif n in memo:
+        return memo[n]
+    else:
+        memo[n] = fibo(n-1, memo) + fibo(n-2, memo)
+        return memo[n]
+    
+print(fibo(500))
